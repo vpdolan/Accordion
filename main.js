a23f1a29e70d39be64a6dbfff5a1aef3a8da2d93
Vinnie Dolan <vpdolan@gmail.com>
2015-10-06 23:46:21 -0400

    tweaked html and main.js to look and perform better

--- main.js
+++ main.js
@@ -1,33 +1,12 @@
 // IIFE
 
-// (function ()) { 
+ //(function ()) {
+ 
 
-/*var modify = function() {
-
-  var parent = $(event.currentTarget.parent);
-  parent.addClass('grow');
-  parent.siblings().removeClass('grow');  
-
-        console.log(this);
-
-  };
-            
-
-
-$('div').on('click', 'a', modify); 
-
-
-
-// }());*/
-
-$(."section").click(function () {
-
-
-$(".section").removeClass("grow");
-
-$(this).addClass("grow");
-
+$( document ).ready(function() {
+  $(".section").click(function() {
+    $(".section").removeClass("grow"); 
+    $(this).addClass("grow"); 
+  });
 });
-
-$('div').on('click', 'a', modify); 
 
