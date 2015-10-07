5e20ff27a58c947c6fba43413425ec847ff09ef1
Vinnie Dolan <vpdolan@gmail.com>
2015-10-06 22:44:11 -0400

    changes to js file

--- main.js
+++ main.js
@@ -2,32 +2,32 @@
 
 // (function ()) { 
 
-function (modify)
+/*var modify = function() {
+
+  var parent = $(event.currentTarget.parent);
+  parent.addClass('grow');
+  parent.siblings().removeClass('grow');  
+
+        console.log(this);
+
+  };
+            
+
+
+$('div').on('click', 'a', modify); 
 
 
 
+// }());*/
+
+$(."section").click(function () {
 
 
+$(".section").removeClass("grow");
 
+$(this).addClass("grow");
 
+});
 
-$('div').on('click', 'a', modify) 
+$('div').on('click', 'a', modify); 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// }());
